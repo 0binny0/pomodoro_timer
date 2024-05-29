482a791c78df51ac14259a15ab430775d8984626
0binny0 <binnyhh@protonmail.com>
2024-05-29 12:04:10 -0700

    set 'test' config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: false,
+    environment: "jsdom",
+    setupFiles: ['./vitest-setup.js'],
+    fakeTimers: {
+      shouldAdvanceTime: true,
+      advanceTimeDelta: 1000,
+    }
+  }
 })
